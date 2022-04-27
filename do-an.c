3970d224990be52dd63a2a750782b64e8a887f1a
Huy Hoang <tlhh232003@gmail.com>
2022-04-27 21:01:31 +0700

    add print_menu function

--- do-an.c
+++ do-an.c
@@ -1,66 +1,54 @@
 #include <stdio.h>
-
-#include <stdlib.h>
-//atoi(), exit()
-
-#include <string.h>
-//strcpy(), strlen()
+#include <stdlib.h> //atoi(), exit()
+#include <string.h> //strcpy(), strlen()
 
 #define MAX 100
 
 void read_menu(char name[][MAX], double cost[], int index[], int* n);
 void open_file(FILE **f, char* file_name, char *mode);
 void input_transaction_code();
+void print_menu(char name[][MAX], double* cost, int number_of_dishes);
 
 int main() {
 	char name[MAX][MAX], transaction_code[100];
 	double cost[MAX];
 	int index[MAX];
 	int number_of_dishes;
-	
+
 	read_menu(name, cost, index, &number_of_dishes);
-	
+
 	input_transaction_code(&transaction_code);
-	
-//	printf("%s\n", &transaction_code);
-//	printf("%d\n", number_of_dishes);
-	for (int i = 0; i < number_of_dishes; i++) {
-		printf("%d\n", index[i]);
-		printf("%s ", name[i]);
-		printf("%.0lf\n", cost[i]);
-		printf("\n");
-	}
-	
+
+	print_menu(name, cost, number_of_dishes);
+
 	return 0;
 }
 
-void read_menu(char name[][MAX], double cost[], int index[], int* n) {
+void read_menu(char name[][MAX], double cost[], int index[], int* i) {
 	FILE* f;
 	open_file(&f, "menu.txt", "r");
 	char line[MAX];
-	int i = 0;
 	size_t ln;
-	
-	fgets(line, MAX, f); *n = atoi(line);
+
 	while (!feof(f)) {
 		fgets(line, MAX, f);
-		index[i] = atoi(line);
-		
+		index[*i] = atoi(line);
+
 		fgets(line, MAX, f);
-		strcpy(name[i], line);
+		strcpy(name[*i], line);
 		//remove \n after name
-		ln = strlen(name[i]) - 1;
-		if (name[i][ln] == '\n')
-    		name[i][ln] = '\0';
-    		
+		ln = strlen(name[*i]) - 1;
+		if (name[*i][ln] == '\n')
+ 		name[*i][ln] = '\0';
+
 		fgets(line, MAX, f);
-		cost[i] = atof(line);
-		
+		cost[*i] = atof(line);
+
 		fgets(line, MAX, f);
-		
-		i++;
+
+		(*i)++;
 	}
-	
+
 	fclose(f);
 }
 void open_file(FILE **file, char* file_name, char *mode) {
@@ -68,7 +56,7 @@ void open_file(FILE **file, char* file_name, char *mode) {
     if (!*file) {
     	printf("Can not open file.\n");
 //		perror("Because");
-		printf("Because can't file menu.txt file");
+		printf("Because can't find menu.txt file");
 		exit(1);
 	}
 }
@@ -76,4 +64,11 @@ void input_transaction_code(char* code) {
 	printf("Type your transaction code: ");
 	scanf("%s", code);
 	system("cls");
+}
+void print_menu(char name[][MAX], double* cost, int number_of_dishes) {
+	printf("STT  NAME:\t\tCOST:\n");
+	for (int i = 0; i < number_of_dishes; i++) {
+		printf("[%d]  ", i+1); puts(name[i]); printf("\t\t\t%.0lf", cost[i]); printf("\n");
+		printf("--------------------------------\n");
+	}
 }
