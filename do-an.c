2b3ab9c5011943714eabe8dfa4f4706fa9196703
Huy Hoang <tlhh232003@gmail.com>
2022-04-30 00:52:03 +0700

    add simplify_string_to_number function

--- do-an.c
+++ do-an.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h> //atoi(), exit()
+#include <stdlib.h> //atof(), exit()
 #include <string.h> //strcpy(), strlen()
+//#include <ctype.h>
 
 #define MAX 100
 
 void read_menu(char name[][MAX], double cost[], int index[], int* n);
 void open_file(FILE **f, char* file_name, char *mode);
 void input_transaction_code();
-void print_menu(char name[][MAX], double* cost, int number_of_dishes);
+void print_menu(char name[][MAX], double cost[], int number_of_dishes, int index[]);
+void order(char name[][MAX], double cost[MAX], int index[MAX]);
+int simplify_string_to_number(char string[]);
 
 int main() {
-	char name[MAX][MAX], transaction_code[100];
+	char name[MAX][MAX], transaction_code[100] = "12345";
 	double cost[MAX];
 	int index[MAX];
 	int number_of_dishes;
 
 	read_menu(name, cost, index, &number_of_dishes);
 
-	input_transaction_code(&transaction_code);
+//	input_transaction_code(&transaction_code);
 
-	print_menu(name, cost, number_of_dishes);
+	print_menu(name, cost, number_of_dishes, index);
+	
+	order(name, cost, index);
 
 	return 0;
 }
@@ -32,7 +37,8 @@ void read_menu(char name[][MAX], double cost[], int index[], int* i) {
 
 	while (!feof(f)) {
 		fgets(line, MAX, f);
-		index[*i] = atoi(line);
+		puts(line);
+		index[*i] = simplify_string_to_number(line);
 
 		fgets(line, MAX, f);
 		strcpy(name[*i], line);
@@ -42,7 +48,7 @@ void read_menu(char name[][MAX], double cost[], int index[], int* i) {
  		name[*i][ln] = '\0';
 
 		fgets(line, MAX, f);
-		cost[*i] = atof(line);
+		cost[*i] = simplify_string_to_number(line);
 
 		fgets(line, MAX, f);
 
@@ -65,10 +71,39 @@ void input_transaction_code(char* code) {
 	scanf("%s", code);
 	system("cls");
 }
-void print_menu(char name[][MAX], double* cost, int number_of_dishes) {
-	printf("STT  NAME:\t\tCOST:\n");
+void print_menu(char name[][MAX], double cost[], int number_of_dishes, int index[]) {
+	printf("ID   NAME:\t\tCOST:\n");
 	for (int i = 0; i < number_of_dishes; i++) {
-		printf("[%d]  ", i+1); puts(name[i]); printf("\t\t\t%.0lf", cost[i]); printf("\n");
+		printf("[%d]  ", index[i]); puts(name[i]); printf("\t\t\t%.0lf", cost[i]); printf("\n");
 		printf("--------------------------------\n");
 	}
 }
+void order(char name[][MAX], double cost[MAX], int index[MAX]) {
+	char input[50];
+	int tmp;
+	int order[100];
+	for (int i = 0; ; i++) {
+		printf("Choose your dishes:\n");
+		fgets(input, sizeof(input), stdin);
+		if (input[0] >= '0' && input[0] <= '9') {
+			tmp = simplify_string_to_number(input);
+			if (tmp == 0) break;
+			order[i] = tmp;
+		}
+	}
+}
+	int simplify_string_to_number(char string[]) {
+		int result;
+		size_t ln;
+		ln = strlen(string) - 1;
+		if (string[ln] == '\n')
+ 		string[ln] = '\0';
+		for (int i = 0; ; i++) {
+			if (string[i] != '\0') {
+				result = 10*result + string[i] - '0'; //'0' == 48
+			} else {
+				break;
+			}
+		}
+		return result;
+	}
