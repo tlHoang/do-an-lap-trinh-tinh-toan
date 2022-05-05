b82691629b0f540c601058ef3bd8fad9905d0ef6
Huy Hoang <tlhh232003@gmail.com>
2022-05-05 13:25:56 +0700

    yc_2

--- do-an.c
+++ do-an.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h> //exit()
-#include <string.h> //strcpy()
+//#include <string.h>
 //#include <ctype.h>
 
 #define MAX 100
@@ -10,11 +10,14 @@ void open_file(FILE **f, char* file_name, char *mode);
 void input_transaction_code();
 void print_menu(char name[][MAX], double cost[], int number_of_dishes, int index[]);
 void order(char name[][MAX], double cost[MAX], int index[MAX], int number_of_dishes);
-int simplify_string_to_number(char string[]);
+int string_to_number(char string[]);
 int string_length(char string[]);
 int string_compare(char string_1[], char string_2[]);
+void string_copy(char des[], char src[]);
 void remove_newline(char string[]);
-int check_order(int tmp[], int n);
+int check_order(int tmp[], char name[][MAX], int n);
+void wait(char name[][MAX], double cost[], int number_of_dishes, int index[], char transaction_code[]);
+int soluong();
 
 int main() {
 	char name[MAX][MAX], transaction_code[100] = "12345";
@@ -23,12 +26,9 @@ int main() {
 	int number_of_dishes;
 
 	read_menu(name, cost, index, &number_of_dishes);
-
 //	input_transaction_code(&transaction_code);
-
-	print_menu(name, cost, number_of_dishes, index);
-
-	order(name, cost, index, number_of_dishes);
+//	print_menu(name, cost, number_of_dishes, index);
+	wait(name, cost, number_of_dishes, index, transaction_code);
 //	for (int i = 0; i < 5; i++) {
 //		printf("%d", name[i]);
 //	}
@@ -41,17 +41,16 @@ void read_menu(char name[][MAX], double cost[], int index[], int* i) {
 	open_file(&f, "menu.txt", "r");
 	char line[MAX];
 
-	printf("==============MENU==============\n\n\n");
 	while (!feof(f)) {
 		fgets(line, MAX, f);
-		index[*i] = simplify_string_to_number(line);
+		index[*i] = string_to_number(line);
 
 		fgets(line, MAX, f);
-		strcpy(name[*i], line);
+		string_copy(name[*i], line);
 		remove_newline(name[*i]);
 
 		fgets(line, MAX, f);
-		cost[*i] = simplify_string_to_number(line);
+		cost[*i] = string_to_number(line);
 
 		fgets(line, MAX, f);
 
@@ -75,9 +74,12 @@ void input_transaction_code(char* code) {
 	system("cls");
 }
 void print_menu(char name[][MAX], double cost[], int number_of_dishes, int index[]) {
+	printf("==============MENU==============\n\n\n");
 	printf("ID   NAME:\t\tCOST:\n");
 	for (int i = 0; i < number_of_dishes; i++) {
-		printf("[%d]  ", index[i]); puts(name[i]); printf("\t\t\t%.0lf", cost[i]); printf("\n");
+		printf("[%d]  ", index[i]);
+		puts(name[i]);
+		printf("\t\t\t%.0lf", cost[i]); printf("\n");
 		printf("--------------------------------\n");
 	}
 }
@@ -85,31 +87,53 @@ void order(char name[][MAX], double cost[MAX], int index[MAX], int number_of_dis
 	char input[50];
 	int order[100];
 	int tmp = 0, i = 0;
-	printf("\n\n\nPress '0' to confirm your order.\n");
-	printf("Press '-1' to undo the last choice.\n");
+	print_menu(name, cost, number_of_dishes, index);
+	printf("\n\n\n[0 0]: confirm\t");
+	printf("[0]: undo\n");
 	printf("Choose your dishes:\n");
 	for (i = 0; ; i++) {
-		fgets(input, sizeof(input), stdin);
+		if (i == 5) {
+			printf("You have reach maximum order. Please confirm or undo...\n");
+			fgets(input, sizeof(input), stdin); 
+			if (input[0] != '0') {
+				i--;
+				continue;
+			}
+		} else {
+			fgets(input, sizeof(input), stdin);
+		}
+		if (string_compare(input, "0 0")) {
+			if (check_order(order, name, i)) {
+				system("cls");
+				break;
+			} else {
+				print_menu(name, cost, number_of_dishes, index);
+				i--;
+				continue;
+			}
+		}
 		if ((input[0] >= '0' && input[0] <= '9') || input[0] == '-') {
-			tmp = simplify_string_to_number(input) - 1;
-			if (tmp == -1) break;
-			if (tmp == -2) {
+			tmp = string_to_number(input) - 1;
+			if (tmp == -1) {
 				i--;
 				order[i] = 0;
 				i--;
 				continue;
 			}
-			if (tmp > number_of_dishes - 1 || tmp < -3) {
+			if (tmp > number_of_dishes - 1 || tmp < -1) {
 				printf("We don't have that dish\n");
 				i--;
 			} else {
+				tmp = soluong(tmp);
 				order[i] = tmp;
 			}
 		} else {
 			remove_newline(input);
 			for (int j = 0; j < number_of_dishes; j++) {
 				if (string_compare(input, name[j])) {
-					order[i] = j;
+					tmp = j;
+					tmp = soluong(tmp);
+					order[i] = tmp;
 					break;
 				}
 				if (j == number_of_dishes - 1) {
@@ -119,11 +143,8 @@ void order(char name[][MAX], double cost[MAX], int index[MAX], int number_of_dis
 			}
 		}
 	}
-	for (int j = 0; j < i; j++) {
-		printf("%s\n", name[order[j]]);
-	}
 }
-int simplify_string_to_number(char string[]) {
+int string_to_number(char string[]) {
 	int result = 0, sign = 1, i = 0;
 	remove_newline(string);
 	if (string[0] == '-') {
@@ -143,6 +164,8 @@ int string_length(char string[]) {
 	return length;
 }
 int string_compare(char string_1[], char string_2[]) {
+	remove_newline(string_1);
+	remove_newline(string_2);
 	int i;
 	for (i = 0; string_1[i] && string_2[i]; i++) {
 		if (string_1[i] == string_2[i] || (string_1[i] ^ 32) == string_2[i]) { //Vi trong ma ascii chu hoa va thuong cach nhau 32 don vi
@@ -157,14 +180,59 @@ int string_compare(char string_1[], char string_2[]) {
 		return 0;
 	}
 }
+void string_copy(char des[], char src[]) {
+	remove_newline(des);
+	remove_newline(src);
+	int i;
+	for (i = 0; src[i] != '\0'; i++) {
+		des[i] = src[i];
+	}
+	des[i] = '\0';
+}
 void remove_newline(char string[]) {
 	size_t ln;
 	ln = string_length(string) - 1;
 	if (string[ln] == '\n')
 	string[ln] = '\0';
 }
-int check_order(int tmp[], int n) {
+int check_order(int tmp[], char name[][MAX], int n) {
+	int check;
+	int sl, dish;
+	system("cls");
+	printf("Please confirm your order\n");
 	for (int i = 0; i < n; i++) {
-		printf("%d", tmp[i]);
+		sl = tmp[i] % 10;
+		dish = tmp[i] / 10;
+		puts(name[dish]);
+		printf("\t\t\t%d\n", sl);
 	}
+	printf("[1]: confirm\t");
+	printf("[0]: back\n");
+	scanf("%d", &check);
+	fflush(stdin);
+	return check;
+}
+void wait(char name[][MAX], double cost[], int number_of_dishes, int index[], char transaction_code[]) {
+	int input = 1;
+	while (1) {
+		printf("[1]: order\t");
+		printf("[0]: end\n");
+		scanf("%d", &input);
+		fflush(stdin);
+		system("cls");
+		if (input == 1) {
+//			print_menu(name, cost, number_of_dishes, index);
+			order(name, cost, index, number_of_dishes);
+		}
+	}
+}
+int soluong(int num) {
+	int sl = 0;
+	while (sl == 0) {
+		printf("so luong:");
+		scanf("%d", &sl);
+		fflush(stdin);
+		num = num*10 + sl;
+	}
+	return num;
 }
